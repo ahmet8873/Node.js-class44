92bdd9561613e9866e116e92898ca98386b22329
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-08-11 16:52:26 +0200

    prep exercise

--- week2/practice-exercises/3-party-time/script.js
+++ week2/practice-exercises/3-party-time/script.js
@@ -1,16 +1,43 @@
-
 /**
  * 3: Party time
- * 
+ *
  * After reading the documentation make a request to https://reservation100-sandbox.mxapps.io/rest-doc/api
  * and print the response to the console. Use async-await and try/catch.
- * 
+ *
  * Hints:
  * - make sure to use the correct headers and http method in the request
  */
 
-function makeReservation() {
-  // YOUR CODE GOES IN HERE
+import fetch from "node-fetch";
+
+async function makeReservation() {
+  try {
+    const apiUrl =
+      "https://reservation100-sandbox.mxapps.io/rest-doc/api/reservations";
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    const requestBody = {
+      name: "ahmet",
+      numberOfPeople: 2,
+    };
+
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  }
 }
 
 makeReservation();
--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,121 @@
-const express = require('express')
-const app = express();
- 
+import express from "express";
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+import fs from "fs";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+// Create
+app.post("/blogs", (req, res) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    res.status(400).json({ error: "Title and content are required." });
+    return;
+  }
+
+  try {
+    fs.writeFileSync(`${title}.json`, JSON.stringify({ title, content }));
+    res.status(201).send("ok");
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the post." });
+  }
+});
+
+// Read one
+app.get("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  if (!fs.existsSync(`${title}.json`)) {
+    res.status(404).json({ error: "This post does not exist!" });
+    return;
+  }
+
+  try {
+    const postContent = fs.readFileSync(`${title}.json`);
+    const post = JSON.parse(postContent);
+    res.status(200).json(post);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while reading the post." });
+  }
+});
+
+// Update
+app.put("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+  const { content } = req.body;
+
+  if (!fs.existsSync(`${title}.json`)) {
+    res.status(404).json({ error: "This post does not exist!" });
+    return;
+  }
+
+  try {
+    fs.writeFileSync(`${title}.json`, JSON.stringify({ title, content }));
+    res.status(200).send("ok");
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the post." });
+  }
+});
+
+// Delete
+app.delete("/blogs/:title", (req, res) => {
+  const { title } = req.params;
+
+  if (!fs.existsSync(`${title}.json`)) {
+    res.status(404).json({ error: "This post does not exist!" });
+    return;
+  }
+
+  try {
+    fs.unlinkSync(`${title}.json`);
+    res.status(200).send("ok");
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the post." });
+  }
+});
+
+// In Node.js, the __dirname variable is only available in CommonJS modules (files that use require and module.exports). If you're using ES Modules (files that use import and export), __dirname is not available by default.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Read all (bonus)
+app.get("/blogs", (req, res) => {
+  try {
+    const files = fs.readdirSync(__dirname);
+    const blogTitles = files
+      .filter((file) => {
+        return (
+          file.endsWith(".json") &&
+          file !== "package.json" &&
+          file !== "package-lock.json"
+        );
+      })
+
+      .map((file) => ({ title: file.replace(".json", "") }));
+
+    res.status(200).json(blogTitles);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while reading blog posts." });
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Blog API is listening at http://localhost:${port}`);
+});
