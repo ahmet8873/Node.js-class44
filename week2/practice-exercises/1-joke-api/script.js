1c53a67d6e545b40d27bbd88cdf026249c0982e8
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-08-11 15:05:32 +0200

    exercises

--- week2/practice-exercises/1-joke-api/script.js
+++ week2/practice-exercises/1-joke-api/script.js
@@ -1,18 +1,36 @@
 /**
  * 1. Chuck Norris programs do not accept input
- * 
+ *
  * `GET` a random joke inside the function, using the API: http://www.icndb.com/api/
- * (use `node-fetch`) and print it to the console. 
+ * (use `node-fetch`) and print it to the console.
  * Make use of `async/await` and `try/catch`
- * 
+ *
  * Hints
  * - To install node dependencies you should first initialize npm
  * - Print the entire response to the console to see how it is structured.
  */
 
-function printChuckNorrisJoke() {
-  // YOUR CODE GOES IN HERE
+import fetch from "node-fetch";
 
+async function printChuckNorrisJoke() {
+  try {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data && data.value) {
+      console.log("full data: ", data);
+      console.log("joke: ", data.value);
+    } else {
+      console.log("Failed to fetch Chuck Norris joke.");
+    }
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+  }
 }
 
 printChuckNorrisJoke();
--- week2/practice-exercises/2-authentication/script.js
+++ week2/practice-exercises/2-authentication/script.js
@@ -1,15 +1,32 @@
-
 /**
  * 2. Authentication
- * 
+ *
  * Using node-fetch make an authenticated request to https://restapiabasicauthe-sandbox.mxapps.io/api/books
  * Print the response to the console. Use async-await and try/catch.
- * 
+ *
  * Hints:
  * - for basic authentication the username and password need to be base64 encoded
  */
-function printBooks() {
-  // YOUR CODE GOES IN HERE
+import fetch from "node-fetch";
+const credentials = "admin:hvgX8KlVEa";
+const base64Credentials = "YWRtaW46aHZnWDhLbFZFYQ==";
+const apiUrl = "https://restapiabasicauthe-sandbox.mxapps.io/api/books";
+
+async function printBooks() {
+  try {
+    const response = await fetch(apiUrl, {
+      headers: { Authorization: `Basic ${base64Credentials}` },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  }
 }
 
 printBooks();
